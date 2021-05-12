63eaf25018df6b536b5b0be4cb16f2c27b9ebb30
Egor <60397322+gostenkoff@users.noreply.github.com>
2021-05-12 13:08:54 +0300

    TypeError handling (#11)
    Added handling the case when "promotionalOffers" is "None".

--- examples/free_games_example.py
+++ examples/free_games_example.py
@@ -16,24 +16,30 @@ def main():
             if image['type'] == 'Thumbnail':
                 game_thumbnail = image['url']
         game_price = game['price']['totalPrice']['fmtPrice']['originalPrice']
-        game_promotions = game['promotions']['promotionalOffers']
-        upcoming_promotions = game['promotions']['upcomingPromotionalOffers']
-        if not game_promotions and upcoming_promotions:
-            # Promotion is not active yet, but will be active soon.
-            promotion_data = upcoming_promotions[0]['promotionalOffers'][0]
-            start_date_iso, end_date_iso = (
-                promotion_data['startDate'][:-1], promotion_data['endDate'][:-1]
-            )
-            # Remove the last "Z" character so Python's datetime can parse it.
-            start_date = datetime.fromisoformat(start_date_iso)
-            end_date = datetime.fromisoformat(end_date_iso)
-            print('{} ({}) will be free from {} to {} UTC.'.format(
-                game_name, game_price, start_date, end_date
-            ))
-        else:
-            print('{} ({}) is FREE now.'.format(
-                game_name, game_price
-            ))
+        try:
+            game_promotions = game['promotions']['promotionalOffers']
+            upcoming_promotions = game['promotions']['upcomingPromotionalOffers']
+            if not game_promotions and upcoming_promotions:
+                # Promotion is not active yet, but will be active soon.
+                promotion_data = upcoming_promotions[0]['promotionalOffers'][0]
+                start_date_iso, end_date_iso = (
+                    promotion_data['startDate'][:-1], promotion_data['endDate'][:-1]
+                )
+                # Remove the last "Z" character so Python's datetime can parse it.
+                start_date = datetime.fromisoformat(start_date_iso)
+                end_date = datetime.fromisoformat(end_date_iso)
+                print('{} ({}) will be free from {} to {} UTC.'.format(
+                    game_name, game_price, start_date, end_date
+                ))
+            else:
+                print('{} ({}) is FREE now.'.format(
+                    game_name, game_price
+                ))
+        except TypeError:
+            pass
+            # or
+            #print('No discounts for this game')
+            # your choice
 
 
 if __name__ == '__main__':
